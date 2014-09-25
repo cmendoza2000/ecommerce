1ed305e2ba85911deafff7df9e44ffd87db24fe0
AAlvAAro <aadelgado@fevaq.net>
2014-09-25 00:14:19 -0500

    migration: add personal info to user model

---
+++ db/migrate/20140925051344_add_info_to_users.rb
@@ -0,0 +1,9 @@
+class AddInfoToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :gender, :string
+    add_column :users, :age, :int
+  end
+end
