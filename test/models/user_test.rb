5df265be9fac5acaee7d06eea0233d67ea286ed9
AAlvAAro <aadelgado@fevaq.net>
2014-09-26 00:00:01 -0500

    add validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,8 +17,20 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
+  test "should not save a repeated email" do
+    user = User.new(email: "email@gmail.com").save
+    copy = User.new(email: "email@gmai.com")
+    assert_not copy.save
+  end
+
   test "should not save user without username" do
     user = User.new
     assert_not user.save
+  end
+
+  test "should not save a repeated username" do
+    user = User.new(username: "aalvaaro").save
+    copy = User.new(username: "aalvaaro")
+    assert_not copy.save
   end
 end
