36862b5aeadbc0dfbc31c26fa4df40f7a0575640
AAlvAAro <aadelgado@fevaq.net>
2014-09-29 22:33:01 -0500

    added basic article model fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925051812) do
+ActiveRecord::Schema.define(version: 20140930033210) do
+
+  create_table "articulos", force: true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "string"
+    t.float    "price"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
