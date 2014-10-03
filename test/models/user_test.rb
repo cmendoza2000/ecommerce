c8654de393940dc06d09b33a01ddbbadf177a028
AAlvAAro <aadelgado@fevaq.net>
2014-10-03 18:03:37 -0500

    create users fixture file

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,36 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without name" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "should not save a repeated name" do
-    user = User.new(first_name: "Alvaro").save
-    copy = User.new(first_name: "Alvaro")
-    assert_not copy.save
-  end
-
-  test "should not save user without email" do
-    user = User.new
-    assert_not user.save
-  end
-
-  test "should not save a repeated email" do
-    user = User.new(email: "email@gmail.com").save
-    copy = User.new(email: "email@gmai.com")
-    assert_not copy.save
-  end
-
   test "should not save user without username" do
-    user = User.new
-    assert_not user.save
+    user = user(:one)
+    user.first_name = nil
+    assert_not user.save, "Saved user without username"
+  end
+  
+  test "should not save user without email" do
+    user = user(:one)
+    user.email= nil
+    assert_not user.save, "Saved user without email"
   end
 
   test "should not save a repeated username" do
-    user = User.new(username: "aalvaaro").save
-    copy = User.new(username: "aalvaaro")
+    user = user(:one)
+    copy = User.new(username: "AAlvAAro")
+    assert_not copy.save, "Saved username that already exists"
+  end
+
+  test "should not save a repeated email" do
+    user = user(:one)
+    copy = User.new(email: "email@gmail.com")
     assert_not copy.save
   end
+
+  test "should delete a user" do
+    user = user(:one)
+    user.destroy
+    assert User.count == 0
+  end
+
 end
