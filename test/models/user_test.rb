e9a4bd5b7058676e1ff5804dccf1b21c2b1895d3
AAlvAAro <aadelgado@fevaq.net>
2014-10-04 15:53:02 -0500

    username and email tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,34 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  # Username's validations
   test "should not save user without username" do
-    user = user(:one)
-    user.first_name = nil
-    assert_not user.save, "Saved user without username"
+    user = users(:one)
+    user.username = nil
+    assert_not user.save
   end
   
-  test "should not save user without email" do
-    user = user(:one)
-    user.email= nil
-    assert_not user.save, "Saved user without email"
-  end
-
   test "should not save a repeated username" do
-    user = user(:one)
+    user = users(:one)
     copy = User.new(username: "AAlvAAro")
-    assert_not copy.save, "Saved username that already exists"
+    assert_not copy.save
+  end
+  
+  test "should not save username with less than 2 characters" do
+    user = users(:one)
+    user.username = "a"
+    assert_not user.save
+  end
+  
+  # Email validations 
+  test "should not save user without email" do
+    user = users(:one)
+    user.email = nil
+    assert_not user.save
   end
 
   test "should not save a repeated email" do
-    user = user(:one)
+    user = users(:one)
     copy = User.new(email: "email@gmail.com")
     assert_not copy.save
   end
 
-  test "should delete a user" do
-    user = user(:one)
-    user.destroy
-    assert User.count == 0
+  test "should not save an email without a valid format" do 
+    user = users(:one)
+    user.email = "email.com"
+    assert_not user.save
   end
 
 end
