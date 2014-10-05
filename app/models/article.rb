61157162f995e50e9d13f18987405beff36d7bb2
AAlvAAro <aadelgado@fevaq.net>
2014-10-05 01:31:59 -0500

    add article model tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 15 }
+  validates :category, presence: true
+  validates :price, presence: true
 end
