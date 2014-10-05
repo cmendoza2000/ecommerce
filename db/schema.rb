0d05965017a0057edc8d3085209e6280b9734dad
AAlvAAro <aadelgado@fevaq.net>
2014-10-05 00:26:15 -0500

    create new article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930033210) do
+ActiveRecord::Schema.define(version: 20141005052519) do
+
+  create_table "articles", force: true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.float    "price"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
