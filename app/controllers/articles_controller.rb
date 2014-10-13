4ad61f336bd1da7e0899abd42230106abf1ef03c
AAlvAAro <aadelgado@fevaq.net>
2014-10-13 18:04:21 -0500

    login user before being leting him publish an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,12 @@ class ArticlesController < ApplicationController
   def show
     #@article = Article.find(params[:id]) 
   end
+
+  def new
+    if user_signed_in?
+    else
+      flash[:notice] = "You need to login to publish an item"
+      redirect_to new_user_session_path
+    end
+  end
 end
